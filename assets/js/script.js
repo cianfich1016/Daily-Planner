56d520e1f2bb93d83837bf8ea93a4862a6742cdb
Rachel Cianfichi <cianfich@gmail.com>
2021-09-24 10:05:05 -0400

    Updated jQuery iterating function to update colors by creating second array and changing syntax of function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var now = moment().format('dddd, MMMM Do');
 $("#currentDay").text(now);
 
+var textArea = $('textarea')
 var text9El = $("#input9");
 var text10El = $("#input10");
 var text11El = $("#input11");
@@ -12,37 +13,31 @@ var text15El = $("#input15");
 var text16El = $("#input16");
 var text17El = $("#input17");
 
-var allTimes = [text9El, text10El, text11El, text12El, text13El, text14El, text15El, text16El, text17El];
+var button9 = $("#button9");
 
-text9El = 9;
-text10El = 10;
-text11El = 11;
-text12El = 12;
-text13El = 13;
-text14El = 14;
-text15El = 15;
-text16El = 16;
-text17El = 17;
+var allElements = [text9El, text10El, text11El, text12El, text13El, text14El, text15El, text16El, text17El];
 
+var allTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+var currentHour = parseInt(moment().format('H'));
+console.log(currentHour)
 
-colors();
-
-function colors(){
-  var currentHour = moment().format('H');
-  for (var i=0; i <= 24; i++){
+textArea.each(function(i){
      if(currentHour < allTimes[i]){
-       allTimes[i].addClass('future')
+       allElements[i].addClass('future')
     }else if (currentHour > allTimes[i]){
-        allTimes[i].addClass('past')
+        allElements[i].addClass('past')
     } else if (currentHour === allTimes[i]){
-        allTimes[i].addClass('present')
+        allElements[i].addClass('present')
+        console.log("hello")
     } else return;
+   
+
     }
-}
+)
 
-//function setStorage9(){
-    //localStorage.setItem("box9", text9Input);
-//};
 
-//button9.addEventListener("click", setStorage9);
+
+//button9.click(function(){
+    //localStorage.setItem("box9", text9El);
+//}
